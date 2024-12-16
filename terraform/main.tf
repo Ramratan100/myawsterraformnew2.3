2ea451f0cd1ef39598ba9d5e9ee1c82f605a0121
Ram ratan yadav <rratan100@gmail.com>
2024-12-16 20:55:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,6 +134,27 @@ resource "aws_security_group" "bastion_sg" {
   vpc_id = aws_vpc.database_vpc.id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
