496f3b4409d6da65bdc5bc4058131cc2a81e3dfa
Ram ratan yadav <rratan100@gmail.com>
2024-12-17 11:20:49 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,8 +5,3 @@ output "bastion_host_public_ip" {
 output "mysql_instance_private_ip" {
   value = aws_instance.mysql_instance.private_ip
 }
-
-output "mysql_load_balancer_dns" {
-  description = "DNS of the MySQL load balancer"
-  value       = aws_lb.mysql_lb.dns_name
-}
